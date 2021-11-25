a72f8cbd2ffe2073a678e198996ea84ae0781f9a
mariaperez22 <95054441+mariaperez22@users.noreply.github.com>
2021-11-25 22:50:00 +0100

    Add files via upload

---
+++ Figure.java
@@ -0,0 +1,32 @@
+package practica1;
+
+
+/**
+ * class Figure.
+ * 
+ * @author LTP 
+ * @version 2020-21
+ */
+
+public abstract class Figure {
+    private double x;
+    private double y;
+    
+    public abstract double area();
+    
+    public Figure(double x, double y) {
+        this.x = x; 
+        this.y = y; 
+    }
+    
+    public boolean equals(Object o) {
+        if (!(o instanceof Figure)) { return false; }
+        Figure f = (Figure) o;
+        return x == f.x && y == f.y;
+    }
+    
+    public String toString() {
+        return "Position: (" + x + ", " + y + ")"; 
+    }
+    
+    }
